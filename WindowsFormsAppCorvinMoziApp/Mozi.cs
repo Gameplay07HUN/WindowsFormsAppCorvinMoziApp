7cacf349a9e24eef5d4589b00626b8b808e27482
Gameplay07HUN <gameplay719@gmail.com>
2023-02-06 08:03:14 +0100

    Add project files.

---
+++ WindowsFormsAppCorvinMoziApp/Mozi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppCorvinMoziApp
+{
+    internal class Mozi
+    {
+        List<Terem> Mterem = new List<Terem>();
+
+    }
+}
---
+++ WindowsFormsAppCorvinMoziApp/Terem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppCorvinMoziApp
+{
+    internal class Terem
+    {
+        string nev;
+        int sorok;
+        int szekekSzama;
+        int ulesek;
+        char nevadokep;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Sorok { get => sorok; set => sorok = value; }
+        public int SzekekSzama { get => szekekSzama; set => szekekSzama = value; }
+        public int Ulesek { get => ulesek; set => ulesek = value; }
+        public char Nevadokep { get => nevadokep; set => nevadokep = value; }
+
+        public Terem(string nev, int sorok, int szekekSzama, int ulesek, char nevadokep)
+        {
+            this.nev = nev;
+            this.sorok = sorok;
+            this.szekekSzama = szekekSzama;
+            this.ulesek = ulesek;
+            this.nevadokep = nevadokep;
+        }
+
+    }
+}
